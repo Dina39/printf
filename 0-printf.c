912247ece8b3f2991da3f1cf9985c8f99e17dec4
Dina39 <dinayacob39@gmail.com>
2022-07-12 01:18:49 -0700

    0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -20,11 +20,13 @@ for (i = 0; format && format[i]; i++)
 if (format[i] == '%')
 {
 if (format[i + 1] == '\0')
-{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+{
+print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 return (-1);
 }
 else
-{	function = get_print_func(format, i + 1);
+{
+function = get_print_func(format, i + 1);
 if (function == NULL)
 {
 if (format[i + 1] == ' ' && !format[i + 2])
@@ -36,12 +38,12 @@ else
 len += function(arguments, buffer, ibuf);
 i += ev_print_func(format, i + 1);
 }
-} i++;
+}
+i++;
 }
 else
 handl_buf(buffer, format[i], ibuf), len++;
-for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-;
+for (ibuf = len; ibuf > 1024; ibuf -= 1024);
 }
 print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 return (len);
